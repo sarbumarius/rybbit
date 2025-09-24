3b13feae88bd82bb6a61312fecf63890aa5bb833
sarbuDEV <73889217+sarbumarius@users.noreply.github.com>
2025-09-25 02:03:59 +0300

    Update next.config.jsdaw
    daw

--- client/next.config.js
+++ client/next.config.js
@@ -1,22 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
   env: {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
     NEXT_PUBLIC_DISABLE_SIGNUP: process.env.NEXT_PUBLIC_DISABLE_SIGNUP,
-    NEXT_PUBLIC_APP_VERSION: process.env.npm_package_version
+    NEXT_PUBLIC_APP_VERSION: process.env.npm_package_version,
   },
-  async rewrites() {
-    if (process.env.NODE_ENV === 'development') {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://backend:3001/api/:path*'
-        }
-      ]
-    }
-    return []
-  }
 };
-
-module.exports = nextConfig;
