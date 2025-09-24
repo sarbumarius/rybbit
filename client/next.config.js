5f3c892328712caf7f73864fc8d1ad6d6a93411a
sarbuDEV <73889217+sarbumarius@users.noreply.github.com>
2025-09-25 01:57:19 +0300

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -6,6 +6,17 @@ const nextConfig = {
     NEXT_PUBLIC_DISABLE_SIGNUP: process.env.NEXT_PUBLIC_DISABLE_SIGNUP,
     NEXT_PUBLIC_APP_VERSION: process.env.npm_package_version
   },
+  async rewrites() {
+    if (process.env.NODE_ENV === 'development') {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://backend:3001/api/:path*'
+        }
+      ]
+    }
+    return []
+  }
 };
 
 module.exports = nextConfig;
